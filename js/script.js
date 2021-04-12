919b5271d46c4b0bf7282861c1721fd71aec5a6d
Elian Xavier <45861861+elianxavier@users.noreply.github.com>
2021-04-12 15:49:37 -0300

    v1.1
    Correção de alguns bugs:
    
    - Linkar o label com o input checkbox
    - O apagar está funcionando perfeitamente
    
    Alterações adicionais:
    
    - Lang agora está em português
    - "const meusItens" foi removida
    - Adicionando os atributos usando setAttribute
    - Função "getNomeDoItem" foi retirada
    - Comentários foram adicionados para um melhor entendimento do código

--- js/script.js
+++ js/script.js
@@ -1,74 +1,110 @@
+
+//cria um array onde vai ser armazenada a lista
 var lista = ["Minha", "lista", "de compras"];
 
+
+//função para pegar um elemento de forma mais simples apesar de desnecessário, não precisa replicar isso no teu projeto
 function element(elemento) { return document.querySelector(elemento); }
 
-function getNomeDoItem()
-{
-    return element("#nomeDoItem");
-}
 
-function addNaLista()
-{
-    const item = getNomeDoItem().value;
-
-    if(item != "")
-    {
-        lista.push(item);
-        getNomeDoItem().value = "";
-
-        render();
-    }
-}
-
+//função que mostra a lista na tela
 function render()
 {
+    /* limpa a seção antes de renderizar tudo de volta.
+    para que a lista não seja mostrada repetidamente */
     element("#meusItens").innerHTML = "";
 
+    //percorre a lista passando o conteúdo e o numero da posição do array
     lista.forEach((nomeDoItem, pos) => {
+
+        //seleciona a seção onde vão ficar os itens da lista
+        const meusItens = element("#meusItens");
+
+        //cria as tags
         const linha = document.createElement("div");
         const check = document.createElement("input");
         const div = document.createElement("div");
         const label = document.createElement("label");
         const btnExcluir = document.createElement("button");
 
+        //adiciona os atributos da div onde o item vai ficar
         linha.setAttribute("class", "linha");
 
-        check.type = "checkbox";
-        check.id = pos;
+        //adiciona os atributos do input checkbox
+        check.setAttribute("type", "checkbox");
+        check.setAttribute("id", pos);
         
+        //adiciona os atributos da div onde o label vai ficar
         div.setAttribute("class", "label");
 
-        label.for = check.id;
+        //adiciona os atributos do label
+        label.setAttribute("for", check.id);
         label.innerHTML = nomeDoItem;
 
-        btnExcluir.innerHTML = "<ion-icon name='close-outline'></ion-icon>";
-        btnExcluir.id = check.id;
+        //adiciona os atributos do botão de excluir
+        btnExcluir.innerHTML = "<ion-icon name='close-outline'></ion-icon>"; //adiciona o ícone de excluir no botão
+        btnExcluir.setAttribute("id", check.id);
         btnExcluir.setAttribute("onclick", "apagar("+ check.id +")");
         btnExcluir.setAttribute("class", "btnExcluir");
 
-        const meusItens = element("#meusItens");
-
-        linha.appendChild(check);
-        div.appendChild(label);
-        linha.appendChild(div);
-        linha.appendChild(btnExcluir);
-        element("#meusItens").appendChild(linha);
+        //organiza as tags filhas dentro das devidas tags mães
+        linha.appendChild(check);//põe a checkbox dentro da linha
+        div.appendChild(label);//põe a label dentro da div
+        linha.appendChild(div);//põe a div dentro da linha
+        linha.appendChild(btnExcluir);//põe o botão de excluir dentro da linha
+        
+        meusItens.appendChild(linha);//põe a linha com todos os elementos dela dentro da seção
     });
 }
 
+
+//mostra a lista logo de cara, para caso tiver algo já adicionado, já ser visualizado
+render();
+
+
+//função que adiciona o item na lista
+function addNaLista()
+{
+    //pega o valor digitado pelo usuário
+    const item = element("#nomeDoItem").value;
+    
+    if(item != "")
+    {
+        /* caso a caixa de texto não estiver vazia,
+        ele adiciona o item no fim da lista */
+        lista.push(item);
+        
+        //esvazia a caixa de texto para que o próximo item possa ser adicionado
+        element("#nomeDoItem").value = "";
+
+        //atualiza a seção para mostrar o novo item no site
+        render();
+    } else
+    {
+        /* caso a caixa de texto esteja vazia,
+        ele emite um alerta para que o usuário a preencha */
+        alert("Digite um item para ser adicionado!!!");
+    }
+}
+
+
+//função que apaga um item da lista
 function apagar(id)
 {
-    console.log("");
-    console.log("Parte 1: ");
-    console.log(lista);
+    //cria uma lista auxiliar
+    let auxLista = [];
 
-    lista = lista.filter((item) => {
-        return item != lista[id];
+    //adiciona na auxiliar todos os itens da lista, menos o item a ser excluído
+    lista.forEach((nomeDoItem, pos) => {
+        if(pos != id && nomeDoItem != "")
+        {
+            auxLista.push(nomeDoItem);
+        }
     });
 
-    console.log("Parte 2: ");
-    console.log(lista);
+    //passa a lista auxiliar para a lista principal
+    lista = auxLista;
+    
+    //atualiza a seção
     render();
 }
-
-render();
